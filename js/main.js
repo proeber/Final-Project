3aa1d5931c58ff8fdd5fb4a6a9958249d76a9945
zach proeber <proeber@wisc.edu>
2014-12-07 21:37:56 -0600

    Merge branch 'master' of https://github.com/proeber/Final-Project

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
   $(document).ready(function(){
   		//global variables
   		var map,
-  		cities,
   		tiles,
       choropleth;
+      var counties = [];
+
+
+   //make search menu sidebar
+   $('#search').sidr({name:'sidr'});   
 
 
   	//new leaflet map
   	map= L.map('map',{
-  		center: [44.7, -90],
+      center: [44.7, -90],
   		zoom: 7,
   		minZoom: 7,
 
   	});
     map.setMaxBounds([[49,-85],[42,-94]]);
+
+
   	//new leaflet tilelayer for background slippy tiles
   	tiles= L.tileLayer('http://{s}.acetate.geoiq.com/tiles/acetate/{z}/{x}/{y}.png',
   	{
@@ -24,21 +30,31 @@
     $.getJSON("data/WI_Counties_geojson.geojson").done(function(choroplethData){
         console.log(choroplethData);
         passChoropleth(choroplethData);
+        for( var i =0; i< 72; i++){
+          counties[i] = choroplethData.features[i];
+
+        }
+
+        geojson = L.geoJson(choroplethData, {
+            onEachFeature: onEachFeature,
+            style: style
+        }).addTo(map);
+
+        
     }).fail(function(){alert("There was a problem loading data")});
+
 
     function passChoropleth(choroplethData){
       choropleth = L.geoJson(choroplethData).addTo(map);
-
+      
     }
 
     //highlight the county
     function highlight(e){
-
       var layer = e.target;
-
       layer.setStyle({
         weight: 5,
-        color: '#666',
+        color: 'yellow',
         dashArray: '',
         fillOpacity: 0.7
       });
@@ -46,9 +62,34 @@
     }
 
     //reset highlight
-    function resetHighlight(e){
-      geojson.resetStyle(e.target);
+    function resetHighlight(e) {
+        geojson.resetStyle(e.target);
     }
 
+
+    function onEachFeature(feature, layer) {
+      layer.on({
+          mouseover: highlight,
+          mouseout: resetHighlight,
+          click: zoomToFeature
+      });
+    }
+
+    function style(feature){
+      return{
+        fillColor: "red",
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+      };
+    }
+
+    function zoomToFeature(e){
+
+      map.fitBounds(e.target.getBounds());
+
+    }
 
   })
