4625febda4429e3b70f2d0cb099a17c419b0023b
tgirgin23 <girgin.timur@gmail.com>
2014-12-15 13:11:10 -0600

    Meh

--- js/main.js
+++ js/main.js
@@ -3,33 +3,36 @@ $(document).ready(function()
 		//global variables
 		var map,
 		tiles,
-    choropleth;
-    var counties = [];
-    var screenHeight;
-    var selected =[];
-    var select = false;
+		choropleth;
+		var counties = [];
+		var screenHeight;
+		var selected =[];
+		var select = false;
+		var layerTarget;
 
-    screenHeight = screen.height;
+		screenHeight = screen.height;
 
-   // $(".container").css("height:"+ screenHeight);
-  // Initializing the dropdowns
-  $(".chosen-select").chosen();
-  //Initializing the table
-  $('#dataTable').tablesorter();
-  
+	 // $(".container").css("height:"+ screenHeight);
+	// Initializing the dropdowns
+	$(".chosen-select").chosen();
+	//Initializing the table
+	$('#dataTable').tablesorter();
+	
 
-  $("#reset").click(function(){
-    history.go(0);
-  });
+	$("#reset").click(function(){
+		history.go(0);
+	});
 
 	//new leaflet maps
 	map = L.map('map',{
-    center: [44.7, -90],
+		center: [44.7, -90],
 		zoom: 7,
 		minZoom: 7,
 
 	});
-  map.setMaxBounds([[49,-85],[42,-94]]);
+	map.setMaxBounds([[49,-85],[42,-94]]);
+
+
 
 
 	//new leaflet tilelayer for background slippy tiles
@@ -38,244 +41,258 @@ $(document).ready(function()
 		attribution: 'Acetate tileset from GeoIQ Data from Wikipedia'
 	}).addTo(map);
 
-  //retrieve choropleth data
-  $.getJSON("data/WI_Counties_geojson.geojson").done(function(choroplethData){
+	//retrieve choropleth data
+	$.getJSON("data/WI_Counties_geojson.geojson").done(function(choroplethData){
 
-      passChoropleth(choroplethData);
+			passChoropleth(choroplethData);
 
-      geojson = L.geoJson(choroplethData, {
-          onEachFeature: onEachFeature,
-          style: style
-      }).addTo(map);
+			geojson = L.geoJson(choroplethData, {
+					onEachFeature: onEachFeature,
+					style: style
+			}).addTo(map);
 
-      var array = geojson._layers;
-      for( key in array){
-        
-        var index = array[key].feature.properties.NAME;
-        counties[index] = array[key];
-      }
+			var array = geojson._layers;
+			for( key in array){
+				
+				var index = array[key].feature.properties.NAME;
+				counties[index] = array[key];
+			}
 
-  }).fail(function(){alert("There was a problem loading data")});
+	}).fail(function(){alert("There was a problem loading data")});
+
+	//Whenever the search button is pressed
+	$( function() {
+		// Getting the input from the user using jQuery
+		$('input#search_button').click( function() {
+
+			select = true;
+			//Storing the input values
+			var county;
+			var type;
+			$('#county_chosen').find('.chosen-single').each(function(){
+			county = $(this).find('span').text();
+			})
+			$('#type_chosen').find('.chosen-single').each(function(){
+			type = $(this).find('span').text();
+			});
+			
+			console.log(county);
+			// Formatting the data type string correctly
+			var correctedType = dataFormat(type);
+			// This goes to the function at line 111 to parse the csv data using the user input
+			parser(correctedType);
+			// Insert code that has to do with data here
+			//highlightSelection(county);
 
 
-  $( function() {
-    // Getting the input from the user using jQuery
-    $('input#search_button').click( function() {
+		});
+	});
 
-      select = true;
-      //Storing the input values
-      var county;
-      var type;
-      $('#county_chosen').find('.chosen-single').each(function(){
-      county = $(this).find('span').text();
-      })
-      $('#type_chosen').find('.chosen-single').each(function(){
-      type = $(this).find('span').text();
-      });
-      
-      console.log(county);
-      // Formatting the data type string correctly
-      var correctedType = dataFormat(type);
-      // This goes to the function at line 111 to parse the csv data using the user input
-      parser(correctedType);
-      // Insert code that has to do with data here
-      //highlightSelection(county);
+	function passChoropleth(choroplethData){
+		choropleth = L.geoJson(choroplethData).addTo(map);
+		
+	}
 
+	//highlight the county
+	function highlight(e){
 
-    });
-  });
+		layerTarget = e.target;
 
-  function passChoropleth(choroplethData){
-    choropleth = L.geoJson(choroplethData).addTo(map);
-    
-  }
+		// layer.bindPopup(layer.feature.properties.NAME);
+		// layer.on('mouseover',function(e){
+		//   this.openPopup();
+		// });
+		// layer.on('mouseout',function(e){
+		//   this.closePopup();
+		// });
 
-  //highlight the county
-  function highlight(e){
+		layerTarget.setStyle({
+			weight: 5,
+			color: 'black',
+			dashArray: '',
+			fillOpacity: 1,
+			fillColor: 'green'
+		});
 
-    var layer = e.target;
-
-    // layer.bindPopup(layer.feature.properties.NAME);
-    // layer.on('mouseover',function(e){
-    //   this.openPopup();
-    // });
-    // layer.on('mouseout',function(e){
-    //   this.closePopup();
-    // });
-
-    layer.setStyle({
-      weight: 5,
-      color: 'black',
-      dashArray: '',
-      fillOpacity: 1,
-      fillColor: 'green'
-    });
-
-  }
+	}
 //highlight the selection based on the county
 //not working yet but hopefully is on the right track
 function highlightSelection(county){
 
-  
-  var layer;
-  var x = 0;
-  //var current = counties[i].properties.NAME;
+	var layer;
+	var x = 0;
+	//var current = counties[i].properties.NAME;
 
-  for(key in counties){
+	for(key in counties){
 
-    //console.log(key == county.trim(), key, county.trim());
+		//console.log(key == county.trim(), key, county.trim());
 
-      if(key === county.trim() )
-    {
-        layer = counties[key];
-        console.log(layer);
+			if(key === county.trim() )
+		{
+				layer = counties[key];
+				console.log(layer);
 
-        layer.setStyle({
-          weight:5,
-          color: 'black',
-          dashArray:'',
-          fillOpacity:1,
-          fillColor:'yellow'
-        });
-    }
-  
-  }
-    
+				layer.setStyle({
+					weight:5,
+					color: 'black',
+					dashArray:'',
+					fillOpacity:1,
+					fillColor:'yellow'
+				});
+		}
+
+	}
+		
 }
 
 
-  //reset highlight
-  function resetHighlight(e) {
+	//reset highlight
+	function resetHighlight(e) {
 
-    if(!select){
-      console.log(i)
-      geojson.resetStyle(e.target);
+		if(!select){
+			console.log(i)
+			geojson.resetStyle(e.target);
 
-    }
-    else{
-      for(var i =0;i<selected.length;i++){
+		}
+		else{
+			for(var i =0;i<selected.length;i++){
 
-        if(e.target.feature.properties.NAME == selected[i].trim()){
+				if(e.target.feature.properties.NAME == selected[i].trim()){
 
-          console.log(e.target.feature.properties.NAME, selected[i]);
+					console.log(e.target.feature.properties.NAME, selected[i]);
 
-          e.target.setStyle({
-            weight:5,
-            color: 'black',
-            dashArray:'',
-           fillOpacity:1,
-           fillColor:'yellow'
+					e.target.setStyle({
+						weight:5,
+						color: 'black',
+						dashArray:'',
+					 fillOpacity:1,
+					 fillColor:'yellow'
 
-          });
-        }
-        else{
-          geojson.resetStyle(e.target);
-        }
-      }
-    }
-    
+					});
+				}
+				else{
+					geojson.resetStyle(e.target);
+				}
+			}
+		}
+		
 
-    
-    
-  }
+		
+		
+	}
 
 
-  function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlight,
-        mouseout: resetHighlight,
-        dblclick: zoomToFeature
-    });
-  }
+	function onEachFeature(feature, layer) {
+		layer.on({
+				mouseover: highlight,
+				mouseout: resetHighlight,
+				dblclick: zoomToFeature
+		});
+		layer.on("click", function(e)
+		{
+			updateTable();
+		})
+	}
 
-  function style(feature){
-    return{
-      fillColor: "red",
-      weight: 2,
-      opacity: 1,
-      color: 'white',
-      dashArray: '3',
-      fillOpacity: 1
-    };
-  }
+	function style(feature){
+		return{
+			fillColor: "red",
+			weight: 2,
+			opacity: 1,
+			color: 'white',
+			dashArray: '3',
+			fillOpacity: 1
+		};
+	}
 
-  function zoomToFeature(e){
+	function zoomToFeature(e){
 
-    var layer = e.target;
+		var layerTarget = e.target;
 
-    map.fitBounds(e.target.getBounds());
+		map.fitBounds(e.target.getBounds());
 
-    e.target.setStyle({
-          weight:5,
-          color: 'black',
-          dashArray:'',
-          fillOpacity:1,
-          fillColor:'yellow'
+		e.target.setStyle({
+					weight:5,
+					color: 'black',
+					dashArray:'',
+					fillOpacity:1,
+					fillColor:'yellow'
 
-    });
-  }
+		});
+	}
 
-  function parser(type)
-  {
-    // Getting the file type 
-    var file = type + ".csv"
-    var path = "/data/csv/" + file;
-    
-    Papa.parse(path, {
-      download: true,
-      delimiter: ',',
-      complete: function(results) {
-          updateDropdown(results.data[0], results.data);
-      }
-    })
-  }
+	function parser(type)
+	{
+		// Getting the file type 
+		var file = type + ".csv"
+		var path = "/data/csv/" + file;
+		
+		Papa.parse(path, {
+			download: true,
+			delimiter: ',',
+			complete: function(results) {
+				updateDropdown(results.data[0], results.data);
+			}
+		})
+	}
 
-  // Format the data type string name
-  function dataFormat(type)
-  {
-    // To lowercase
-    var lowerType = type.toLowerCase();
-    // Replacing spaces with underscores
-    var typeFormat = lowerType.replace(/ /g, "_");
-    return typeFormat;
-  }
+	// Format the data type string name
+	function dataFormat(type)
+	{
+		// To lowercase
+		var lowerType = type.toLowerCase();
+		// Replacing spaces with underscores
+		var typeFormat = lowerType.replace(/ /g, "_");
+		return typeFormat;
+	}
 
-  // Updating the dropdown menus with the correct data from the csv files
-  function updateDropdown(years, data)
-  {
-    var bool = false;
-    var index =-1;
+	// Updating the dropdown menus with the correct data from the csv files
+	function updateDropdown(years, data)
+	{
+		var bool = false;
+		var index =-1;
+		$('#county').empty();
+		for(j = 1; j < data.length; j++)
+		{
+			bool = false;
+			for(k = 0; k < data[j].length; k++)
+			{
+				if(data[j][k] == "TRUE" && bool == false)
+				{
+					// Setting boolean to true when found TRUE in current row
+					bool = true;
+					var option = '';
+					option = '<option value="' + data[j][0] + '">' + data[j][0] + '</option>';
+					$('#county').append(option);
+					$('#county').trigger('chosen:updated');
+					//console.log(data[j][0]);
 
-    $('#county').empty();
-    for(j = 1; j < data.length; j++)
-    {
-      bool = false;
-      for(k = 0; k < data[j].length; k++)
-      {
-        if(data[j][k] == "TRUE" && bool == false)
-        {
-          // Setting boolean to true when found TRUE in current row
-          bool = true;
-          var option = '';
-          option = '<option value="' + data[j][0] + '">' + data[j][0] + '</option>';
-          $('#county').append(option);
-          $('#county').trigger('chosen:updated');
-          //console.log(data[j][0]);
+					index +=1;
+					//call the highlight option for a selection from the dropdowns
+					console.log(data[j][0]);
+					selected[index] = data[j][0].trim();
+					highlightSelection(data[j][0]);
+				}
+			}
+		}
+		var option = '';
+		for (i = 1; i < years.length; i++){
+			 option += '<option value="'+ years[i] + '">' + years[i] + '</option>';
+		}
+		$('#year').append(option);
+		$('#year').trigger('chosen:updated');
+	}
 
-          index +=1;
-          //call the highlight option for a selection from the dropdowns
-          console.log(data[j][0]);
-          selected[index] = data[j][0].trim();
-          highlightSelection(data[j][0]);
-        }
-      }
-    }
-    //$('#year_chosen').empty();
-    var option = '';
-    for (i = 1; i < years.length; i++){
-       option += '<option value="'+ years[i] + '">' + years[i] + '</option>';
-    }
-    $('#year').append(option);
-    $('#year').trigger('chosen:updated');
-  }
+	function updateTable(years, data)
+	{
+		layerTarget.on('click', function(e)
+		{
+			console.log(e.target.feature.properties.NAME);
+			$( ".tBody" ).empty();
+			for(i = 1; i < years.length; i++)
+			{
+				console.log(years.length);
+			}
+		});
+	}
 })
